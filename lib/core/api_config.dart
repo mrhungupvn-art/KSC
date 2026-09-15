b489d29d2de685bcca9d7a34540c47ebe180b634
mrhungupvn-art <mr.hung.upvn@gmail.com>
2026-09-15 15:10:11 +0700

    Delete lib directory

--- lib/core/api_config.dart
+++
@@ -1,6 +0,0 @@
-/// Đổi đúng domain thật của bạn ở đây (không có dấu / ở cuối).
-/// api/ trên host là REST API cho App KSC, tách biệt với admin/.
-const String kApiBaseUrl = 'https://ksc.foodkcn.com/api';
-
-/// Domain gốc, dùng để ghép URL ảnh trả về từ server (vd: /uploads/jobs/12/ph_xxx.jpg)
-const String kMediaBaseUrl = 'https://ksc.foodkcn.com';
